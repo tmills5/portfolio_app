0d1652d7e9131230f37fdc90deb11bcf3b86b698
Merle Tyson Mills <tysonmills5@gmail.com>
2022-10-17 14:50:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import TopNavbar from './components/TopNavbar';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopNavbar />
+        <Routes>
+
+        </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/TopNavbar.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+function TopNavbar() {
+  return(
+    <>
+
+    </>
+  )
+}
+export default TopNavbar;
