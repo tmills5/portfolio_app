f2f91cf085e82eb9bdb2d4dde1c0496556ed7d8a
Merle Tyson Mills <tysonmills5@gmail.com>
2022-10-21 11:38:29 -0500

    contact info to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,24 +8,24 @@ function Footer() {
       <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
         {/* <!-- Left --> */}
         <div class="me-5 d-none d-lg-block">
-          <span>Get connected with us on social networks:</span>
+          <span>Thank you for visiting! Let's get connected: </span>
         </div>
         {/* <!-- Left --> */}
     
         {/* <!-- Right --> */}
         <div>
-          <a href="/" class="me-4 text-reset">
+          {/* <a href="/" class="me-4 text-reset">
             <i class="fab fa-facebook-f"></i>
-          </a>
+          </a> */}
           <a href="/" class="me-4 text-reset">
             <i class="fab fa-twitter"></i>
           </a>
           <a href="/" class="me-4 text-reset">
             <i class="fab fa-google"></i>
           </a>
-          <a href="/" class="me-4 text-reset">
+          {/* <a href="/" class="me-4 text-reset">
             <i class="fab fa-instagram"></i>
-          </a>
+          </a> */}
           <a href="/" class="me-4 text-reset">
             <i class="fab fa-linkedin"></i>
           </a>
@@ -46,7 +46,7 @@ function Footer() {
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
               {/* <!-- Content --> */}
               <h6 class="text-uppercase fw-bold mb-4">
-                <i class="fas fa-gem me-3"></i>Company name
+                <i class="fas fa-gem me-3"></i>tmillsdev
               </h6>
               <p>
                 Here you can use rows and columns to organize your footer content. Lorem ipsum
@@ -59,19 +59,19 @@ function Footer() {
             <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
               {/* <!-- Links --> */}
               <h6 class="text-uppercase fw-bold mb-4">
-                Products
+                Projects
               </h6>
               <p>
-                <a href="#!" class="text-reset">Angular</a>
+                <a href="#!" class="text-reset">MBH</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">React</a>
+                <a href="#!" class="text-reset">ParkIt</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">Vue</a>
+                <a href="#!" class="text-reset">CocktailCompanion</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">Laravel</a>
+                <a href="#!" class="text-reset">LitDistrict</a>
               </p>
             </div>
             {/* <!-- Grid column --> */}
@@ -101,13 +101,13 @@ function Footer() {
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
               {/* <!-- Links --> */}
               <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-              <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+              <p><i class="fas fa-home me-3"></i> Starkville, MS 39759, US</p>
               <p>
                 <i class="fas fa-envelope me-3"></i>
-                info@example.com
+                TMillsDev@gmail.com
               </p>
-              <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-              <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+              <p><i class="fas fa-phone me-3"></i> 706-380-0637</p>
+              {/* <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p> */}
             </div>
             {/* <!-- Grid column --> */}
           </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from './App';
 
 
