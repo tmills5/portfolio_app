5d7b5209492ccc79ac6b54d581fd61ee6ba412bf
Merle Tyson Mills <tysonmills5@gmail.com>
2022-12-03 22:00:06 -0600

    photo collage about

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,22 @@
-import SplitScreen from '../layout/SplitScreen';
 import TyCoffee from '../assets/images/TyCoffee.jpeg';
+import TyFireSelfie from '../assets/images/TyFireSelfie.JPG';
+import TyLoveUALatte from '../assets/images/TyLoveUALatte.jpeg';
+import { ReactPhotoCollage } from "react-photo-collage";
 
+const setting = {
+  width: '100vw',
+  height: ['350px', '300px'],
+  layout: [1, 4],
+  photos: [
+    { source: {} },
+    { source: `${TyCoffee}` },
+    { source: `${TyFireSelfie}` },
+    { source: `${TyLoveUALatte}` },
+    { source: 'url/image-5.jpg' },
+    { source: 'url/image-6.jpg' },
+  ],
+  showNumOfRemainingPhotos: true
+};
 
 
 
@@ -8,7 +24,7 @@ function About() {
   
   return (
     <>
-    <div className="d-flex p-4 justify-content-between about-container">
+    {/* <div className="d-flex p-4 justify-content-between about-container">
       <img 
         src={TyCoffee}
         height={500}
@@ -19,7 +35,8 @@ function About() {
         src={TyCoffee}
         height={500}
         alt='' />
-    </div>
+    </div> */}
+      <ReactPhotoCollage {...setting} />
     </>
   )
 }
