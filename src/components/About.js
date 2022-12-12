ec0a173c17744ff96dd8944ff780fc28806dbc14
Merle Tyson Mills <tysonmills5@gmail.com>
2022-12-12 12:05:29 -0600

    implement flex box

--- src/components/About.js
+++ src/components/About.js
@@ -2,28 +2,39 @@ import TyCoffee from '../assets/images/TyCoffee.jpeg';
 import TyFireSelfie from '../assets/images/TyFireSelfie.JPG';
 import TyLoveUALatte from '../assets/images/TyLoveUALatte.JPG';
 import { Fade, Slide } from 'react-awesome-reveal';
+import { LoremIpsum } from "react-lorem-ipsum";
 
 // going outside my comfort zone is how i succeed and i love to push my own limits
 
 function About() {
   
   return (
-    <>
-    <div className="d-flex p-4 justify-content-between about-container">
-      <Fade duration='2000'>
-      <img 
-        src={TyFireSelfie}
-        height={500}
-        alt='' />
-      </Fade>
-      <Fade duration='2000'>
-      <img 
-        src={TyLoveUALatte}
-        height={500}
-        alt=''/>
-      </Fade>
+    <div className='about-container'>
+      <header className='about-section-header'>
+        <div className='about-section-header-inner-container'>
+          <h3>fjkdlislkdklfjka;slkdfjfkdskljflkjsdl;fkj; a;slkdjfjkdka;kj a;lskdj;kljf; lkjdlkjf;ldkjf</h3>
+        </div>
+        {/* <span className='about-hero-paragraph-wrapper'>
+          <p>fjdfkdjflkajls;dkjf;laksdjf;lkajs</p>
+        </span> */}
+      </header>
     </div>
-    </>
+    // <>
+    // <div className="d-flex p-4 justify-content-between about-container">
+    //   <Fade duration='2000'>
+    //   <img 
+    //     src={TyFireSelfie}
+    //     height={500}
+    //     alt='' />
+    //   </Fade>
+    //   <Fade duration='2000'>
+    //   <img 
+    //     src={TyLoveUALatte}
+    //     height={500}
+    //     alt=''/>
+    //   </Fade>
+    // </div>
+    // </>
   )
 }
 
