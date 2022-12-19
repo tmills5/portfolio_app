9853c3511d4a08abe60f63790e353e4ca6285baf
Merle Tyson Mills <tysonmills5@gmail.com>
2022-12-19 09:00:15 -0600

    landing is now grid

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,7 +7,7 @@ function Landing() {
     return(
         <div className="landing-parent-div">
             <a href='/home'>
-                <Fade duration='3000' class>
+                <Fade duration='3000' className='logo-div'>
                     <img src={logoBlack} alt=""/>
                 </Fade>
             </a>
