f866a3267656187d37a8a28305e9a99aad9ed2fe
Merle Tyson Mills <tysonmills5@gmail.com>
2022-12-19 10:29:14 -0600

    hero pic centered. responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,73 +6,75 @@ import {Fade, Slide} from 'react-awesome-reveal';
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
 
 function Home() {
-
     return(
-    <MDBContainer className='bg-black home-main'>
-        <MDBRow evenly>
-            <MDBCol size='4'>
-                <Fade duration='3400'>
-                    <div className='bg-image'>
-                    <img 
-                        className='img-fluid hero-pic'
-                        src={TyHeadshot} 
-                        alt="hero-pic"
-                    />
-                    <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}></div>
-                    </div>
-                </Fade>
-            </MDBCol>
-            <MDBCol size='8' className='home-right-text'>
-               <div className='home-title'>Tyson Mills</div>
-               <Slide direction='right' du><h5>Full Stack Software Engineer - Based in Mississippi, USA</h5></Slide>
+      <MDBContainer className='container-md home-main'>
+        <MDBRow>
+          <MDBCol className="col-sm-6 col-lg-6">
+            {/* <Fade duration='3400'> */}
+              <div className='bg-image'>
+                <img 
+                  className='img-fluid hero-pic'
+                  src={TyHeadshot} 
+                  alt="hero-pic"
+                />
+              </div>
+            {/* </Fade> */}
+          </MDBCol>
+
+          <MDBCol className='col-sm-6 col-lg-6 home-right-text'>
+            <div className='home-title'>Tyson Mills</div>
+            <Slide direction='right' du><h5>Full Stack Software Engineer - Based in Mississippi, USA</h5></Slide>
             <hr/>
-               <Slide direction='up'><p>
-                From career firefighter to software developer...<br/>I bring a strong sense of professionalism, adaptability, and resourcefulness to all of my projects. 
-                </p></Slide> 
+            <Slide direction='up'><p>
+              From career firefighter to software developer...<br/>I bring a strong sense of professionalism, adaptability, and resourcefulness to all of my projects. 
+              </p></Slide>
+
             <hr/>
+
             <div className='social-links'>
-            <MDBBtn
-          // rippleColor="light"
-          color='link'
-          floating
-          size="lg"
-          className='text-dark m-1'
-          href='https://www.linkedin.com/in/tmillsdev'
-          role='button'
-          >
-            <MDBIcon fab className='fa-linkedin' size='2x'/>
-        </MDBBtn>
-        <MDBBtn
-          rippleColor="dark"
-          color='link'
-          floating
-          size="lg"
-          className='text-dark m-1'
-          href='https://www.twitter.com/tysonmillsdev'
-          role='button'
-          >
-            <MDBIcon fab className='fa-twitter' size='2x'/>
-        </MDBBtn>
-        <MDBBtn
-          rippleColor="dark"
-          color='link'
-          floating
-          size="lg"
-          className='text-dark m-1'
-          href='https://github.com/tmills5'
-          role='button'
-          >
-            <MDBIcon fab className='fa-github' size='2x'/>
-        </MDBBtn>
-        </div>
-            </MDBCol>
+              <MDBBtn
+                color='link'
+                floating
+                size="lg"
+                className='text-dark m-1'
+                href='https://www.linkedin.com/in/tmillsdev'
+                role='button'
+              >
+                <MDBIcon fab className='fa-linkedin' size='2x'/>
+              </MDBBtn>
+
+              <MDBBtn
+                rippleColor="dark"
+                color='link'
+                floating
+                size="lg"
+                className='text-dark m-1'
+                href='https://www.twitter.com/tysonmillsdev'
+                role='button'
+              >
+                <MDBIcon fab className='fa-twitter' size='2x'/>
+              </MDBBtn>
+
+              <MDBBtn
+                rippleColor="dark"
+                color='link'
+                floating
+                size="lg"
+                className='text-dark m-1'
+                href='https://github.com/tmills5'
+                role='button'
+              >
+                <MDBIcon fab className='fa-github' size='2x'/>
+              </MDBBtn>
+            </div>
+          </MDBCol>
           
         </MDBRow>
-        <div className="copyright">
-        &copy; {new Date().getFullYear()} Copyright:{' '}
-        TMillsDev
-      </div>
-    </MDBContainer>
+          <div className="copyright">
+            &copy; {new Date().getFullYear()} Copyright:{' '}
+            TMillsDev
+          </div>
+      </MDBContainer>
     )
 }
 
