e682c3677501510dec8bf14618155c4153c01d50
Merle Tyson Mills <tysonmills5@gmail.com>
2022-12-22 16:59:11 -0600

    contact link

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -4,6 +4,7 @@ function ContactSection() {
       <section id="contact">
         <h2>Contact Me!</h2>
         <p>I'm always interested in new opportunities, hearing about what you have going on, or just connecting towards a common goal.</p>
+          <a href='/contact'>Send Me a Message</a>
       </section>
     </div>
 
