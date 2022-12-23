1eee6345a9acd64971d4afcc88672c8abd5748ff
Merle Tyson Mills <tysonmills5@gmail.com>
2022-12-22 21:57:59 -0600

    added parkit. need to reverse

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,4 +1,6 @@
-import MBHScreenShot from '../assets/images/MBHScreenShot.png'
+import MBHScreenShot from '../assets/images/MBHScreenShot.png';
+import ParkItScreenShot from '../assets/images/ParkItScreenShot.png';
+
 
 function ProjectsSection () {
   return(
@@ -9,7 +11,7 @@ function ProjectsSection () {
         </h2>
         <article>
           <div className='project-text'>
-            <h4>Latest Projects</h4>
+            <h4>Latest Project</h4>
             <h3>Mississippi BrewHouse</h3>
             <p className='blackbox'>The Mississippi BrewHouse is a Single Page Application that allows users to login and explore the local breweries in Mississippi. The user has the ability to leave comments on visited   breweries as well as track those comments in their user profile. sdfgsdfgsdfgsdfgjdtus;ldhjkgf; lkjas;lkjfg ;lkajsdflkj ;lkajsdf;lkj lkajsdf;lkja slkj lkj laskdjfsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfg sdfgsdfgsdfgsdfgsdfgsdfgsdfgsdfg</p>
             <h4>Technologies Used:</h4>
@@ -22,6 +24,23 @@ function ProjectsSection () {
               </ul>
           </div>
           <img src={MBHScreenShot} alt='MBH homepage' className='project-img'/>
+        </article>
+        <br/>
+        <article>
+          <div className='project-text'>
+            <h4>Outdoor Fun!</h4>
+            <h3>ParkIt</h3>
+            <p className='blackbox'>ParkIt is an application designed for the outdoor enthusiast in mind. The user has the ability to log in and search for National Parks to gather requisite information. The   "Admin" has full CRUD functionality.</p>
+            <h4>Technologies Used:</h4>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Ruby on Rails</li>
+                <li>PostgreSQL</li>
+                <li>Material-UI</li>
+              </ul>
+          </div>
+          <img src={ParkItScreenShot} alt='parkIt homepage' className='project-img'/>
         </article>
       </section>
     </div>
