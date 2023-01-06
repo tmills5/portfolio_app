adef9cf2bd6250c69720c0360e6880d108b4f5a3
Merle Tyson Mills <tysonmills5@gmail.com>
2023-01-05 22:19:22 -0600

    new color palette

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -5,9 +5,11 @@ function IntroSection() {
   return(
     <section id="intro">
       <p className='name'>Welcome, I'm <span>Tyson.</span></p>
-      <h2>I'm a Web Developer</h2>
-      <p>Full Stack Software Engineer - Based in Mississippi, USA</p>
-      <p>From career firefighter to software developer...I bring a strong sense of professionalism, adaptability, and resourcefulness to all of my work.</p>
+      <h2>I'm a Web Developer.</h2>
+      <p>Full Stack Software Engineer | Based in Mississippi, USA</p>
+      <p>
+        From career firefighter to software developer- I bring a strong sense of professionalism, adaptability, and resourcefulness to all of my work.
+      </p>
     </section>
   )
 };
