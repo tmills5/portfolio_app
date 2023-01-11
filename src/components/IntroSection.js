10e326a7dee5febf6d530567fb27fe43a6578451
Merle Tyson Mills <tysonmills5@gmail.com>
2023-01-10 20:40:47 -0600

    animations on intro

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Slide } from 'react-awesome-reveal';
+import { Slide, Fade } from 'react-awesome-reveal';
 
 function IntroSection() {
   
   return(
     <section id="intro">
-      <p className='name'>Welcome, I'm <Slide triggerOnce="true"><span>Tyson.</span></Slide></p>
-      <Slide direction='right'><h2>I'm a Web Developer.</h2></Slide>
+      <Fade cascade damping='1'>
+        <p className='name'>Welcome, I'm <Slide><span>Tyson.</span></Slide></p>
+        <Slide direction='up' ><h2>I'm a Web Developer.</h2></Slide>
+      </Fade>
       <p>Full Stack Software Engineer - Based in Mississippi, USA</p>
       <p>
         From career firefighter to software developer - I bring a strong sense of professionalism, adaptability, and resourcefulness to all of my work.
