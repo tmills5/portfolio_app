bc3d809737a7f8bafc6cbe2db572ef55cd4b6a74
Merle Tyson Mills <tysonmills5@gmail.com>
2023-02-07 14:30:46 -0600

    done but pdf issue

--- src/components/ResumeViewer.js
+++ src/components/ResumeViewer.js
@@ -1,13 +1,11 @@
-import { Document,Page } from 'react-pdf/dist/esm/entry.webpack';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import test from '../assets/documents/TysonMillsResumeFullStack.docx.pdf';
 
 function ResumeViewer() {
   return (
-    <div id=".resume-pdf-container">
-      <Document className='resume-pdf' file={test}>
-        <Page pageNumber={1} />
-      </Document>
-    </div>
+    <Document file={test}>
+      <Page pageNumber={1} />
+    </Document>
   );
 }
 
