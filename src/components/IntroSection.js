a300a99c0a66deefd28cf5a495965e6d7b397be8
Merle Tyson Mills <tysonmills5@gmail.com>
2023-02-07 13:39:53 -0600

    done with intro

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -4,21 +4,22 @@ import { Slide, Fade } from 'react-awesome-reveal';
 
 function IntroSection() {
   
-  return(\
+  return(
     <div className='intro-section'>
     <section id="intro">
       <article>
       <div className='intro-text'>
         <Fade cascade damping='1' triggerOnce='true'>
           <p className='name'>Welcome, I'm <Slide triggerOnce='true'><span>Tyson.</span></Slide></p>
-          <Slide direction='up' ><h2>I'm a Web Developer.</h2></Slide>
+          <Slide triggerOnce='true'><h2>I'm a Web Developer.</h2></Slide>
         </Fade>
         <p>Full Stack Software Engineer - Based in Mississippi, USA</p>
         <p>
           From career firefighter to software developer - I bring a strong sense of professionalism, adaptability, and resourcefulness to all of my work.
         </p>
+        <div className="arrow"></div>
       </div>
-      {/* <img src={headshot} alt="Tyson's face" /> */}
+      <img src={headshot} alt="Tyson's face" />
       </article>
     </section>
     </div>
