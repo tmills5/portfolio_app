ba51855afea0452ae7f117d17250c34bbd939563
Merle Tyson Mills <tysonmills5@gmail.com>
2023-02-08 12:55:20 -0600

    try

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
   const [name, setName] = useState('');
@@ -21,161 +21,101 @@ const Form = () => {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    
+
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
       .then(function(response) {
         console.log('SUCCESS!', response.status, response.text);
       }, function(error) {
         console.log('FAILED...', error);
       });
+      e.target.reset();
+    }
+
 
   return (
 
-<div class="contact">
-<div id="container">
-  <h5>Got an idea for a project?</h5>
-  <form onSubmit={handleFormSubmit} id="contact_form">
-    <div class="form-name">
-      <label for="name"></label>
-      <input 
-        type="text" 
-        placeholder="name" 
-        name="name" 
-        id="name_input"
-        onChange={e => setName(e.target.value)}
-        value={name}
-        required
-      />
+    <div class="contact">
+      <div id="container">
+        <h5>Got an idea for a project?</h5>
+        <form onSubmit={handleFormSubmit} id="contact_form">
+          <div class="form-name">
+            <label for="name"></label>
+              <input 
+                type="text" 
+                placeholder="name" 
+                name="name" 
+                id="name_input"
+                onChange={e => setName(e.target.value)}
+                value={name}
+                required
+              />
+          </div>
+          <div class="email">
+            <label for="email"></label>
+              <input 
+                type="email" 
+                placeholder="e-mail" 
+                name="email" 
+                id="email_input" 
+                onChange={e => setEmail(e.target.value)}
+                value={email}
+                required
+              />
+          </div>
+          <div class="company">
+            <label for="company"></label>
+              <input 
+                type="text" 
+                placeholder="company" 
+                name="company" 
+                id="company_input"
+                onChange={e => setCompany(e.target.value)}
+                value={company}
+              />
+          </div>
+          <div class="phone">
+            <label for="phone"></label>
+              <input 
+                type="text" 
+                placeholder="phone" 
+                name="phone" 
+                id="phone_input" 
+                onChange={e => setPhone(e.target.value)}
+                value={phone}
+              />
+          </div>
+          <div class="url">
+            <label for="url"></label>
+              <input 
+                type="text" 
+                placeholder="website" 
+                name="url" 
+                id="url_input" 
+                onChange={e => setUrl(e.target.value)}
+                value={url}
+                required
+              />
+          </div>
+          <div class="message">
+            <label for="message"></label>
+              <textarea 
+                name="message" 
+                placeholder="message"
+                id="message_input" 
+                cols="30" 
+                rows="5" 
+                onChange={e => setMessage(e.target.value)}
+                value={message}
+                required 
+              />
+          </div>
+          <div class="submit">
+            <input type="submit" value="submit" id="form_button"/>
+          </div>
+      </form>
     </div>
-    <div class="email">
-      <label for="email"></label>
-      <input 
-      type="email" 
-      placeholder="e-mail" 
-      name="email" 
-      id="email_input" 
-      onChange={e => setEmail(e.target.value)}
-      value={email}
-      required
-    />
-    </div>
-    <div class="company">
-      <label for="company"></label>
-      <input 
-        type="text" 
-        placeholder="company" 
-        name="company" 
-        id="company_input"
-        onChange={e => setCompany(e.target.value)}
-        value={company}
-      />
-    </div>
-    <div class="phone">
-      <label for="phone"></label>
-      <input 
-        type="text" 
-        placeholder="phone" 
-        name="phone" 
-        id="phone_input" 
-        onChange={e => setPhone(e.target.value)}
-        value={phone}
-      />
-    </div>
-    <div class="url">
-      <label for="url"></label>
-      <input 
-        type="text" 
-        placeholder="website" 
-        name="url" 
-        id="url_input" 
-        onChange={e => setUrl(e.target.value)}
-        value={url}
-        required
-      />
-    </div>
-    <div class="message">
-      <label for="message"></label>
-      <textarea 
-        name="message" 
-        placeholder="message"
-        id="message_input" 
-        cols="30" 
-        rows="5" 
-        onChange={e => setMessage(e.target.value)}
-        value={message}
-        required 
-      />
-    </div>
-    <div class="submit">
-      <input type="submit" value="submit" id="form_button"/>
-    </div>
-  </form>
 </div>
-</div>
 
-
-
-
-//     <div id="contact-form">
-
-
-//         <input
-//           className="form-input"
-//           type="textarea"
-//           placeholder="Tell me about the product we can create..." 
-//           value={message} 
-//           onChange={e => setMessage(e.target.value)} 
-//         />
-
-//       <input
-//         className="form-input"
-//         contrast
-//         // label="Name" 
-//         type="text" 
-//         placeholder="Your Name" 
-//         value={name} 
-//         onChange={e => setName(e.target.value)} 
-//       />
-
-//       <input
-//         className="form-input"
-//         contrast
-//         // label="Subject"
-//         placeholder='Your Company'
-//         type="text" 
-//         value={company} 
-//         onChange={e => setCompany(e.target.value)} 
-//       />
-//       <input 
-//         className="form-input"
-//         contrast
-//         id='typeEmail'
-//         type='email'
-//         // label='Email input'
-//         placeholder="Your Email Address" 
-//         value={email} 
-//         onChange={e => setEmail(e.target.value)} 
-//       />
-
-//       <input 
-//         className="form-input" 
-//         contrast 
-//         type="url" 
-//         placeholder="Your website URL"
-//         value={url}
-//         onChange={e => setUrl(e.target.value)}
-//       />
-
-// <button type="button" class="btn btn-primary">Button</button>
-// <MDBBtn>Button</MDBBtn>
-//       <MDBBtn>Get it Touch</MDBBtn>
-//       <MDBBtn>Reset</MDBBtn>
-
-
-//       <form>
-//         <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
-//       </form>
-//     </div>
   );
 }
 
