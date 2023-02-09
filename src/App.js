49cd2bd318340eadee4ba90e26f31a9761828a78
Merle Tyson Mills <tysonmills5@gmail.com>
2023-02-09 09:03:38 -0600

    set up router again

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import { Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
 import Nav from './components/Nav';
 import Home from './components/Home';
-// import ProjectsPage from './components/ProjectsPage';
-// import About from './components/About';
+import ProjectsSection from './components/ProjectsSection';
+import About from './components/About';
 import ContactPage from './components/ContactPage';
 // import ResumeViewer from './components/ResumeViewer';
 
 
 
 function App() {
+
   return (
     <div className="App">
       {/* not rendering the navbar on landing page */}
       {window.location.pathname !== '/' ? <Nav /> : null}
+
       <Routes>
         <Route exact path='/' element={<Landing />} />
         <Route exact path='/home' element={<Home />} />
-        {/* <Route exact path='/projects' element={<ProjectsPage />} /> */}
-        {/* <Route exact path='/about' element={<About />} /> */}
+        <Route exact path='/projects' element={<ProjectsSection />} />
+        <Route exact path='/about' element={<About />} />
         <Route exact path='/contact' element={<ContactPage />} />
         {/* <Route exact path='/resume' element={<ResumeViewer />} /> */}
 
