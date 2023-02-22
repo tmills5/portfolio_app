a63c651a7a38f47e5716a29c7b0e5860d3531234
Merle Tyson Mills <tysonmills5@gmail.com>
2023-02-22 09:31:26 -0600

    added in progress

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,6 +1,8 @@
 import MBHScreenShot from '../assets/images/MBHScreenShot.png';
 import ParkItScreenShot from '../assets/images/ParkItScreenShot.png';
+import FireSphereScreenShot from '../assets/images/FireSphereScreenShot.png';
 import { Zoom } from 'react-awesome-reveal';
+
 
 //! deploy and include link
 
@@ -11,6 +13,7 @@ function ProjectsSection () {
         <h2>
           Projects
         </h2>
+        {/* Mississippi BrewHouse */}
         <Zoom triggerOnce='true'>
         <article>
           <div className='project-text'>
@@ -36,6 +39,7 @@ function ProjectsSection () {
         </article>
         </Zoom>
         <br/>
+        {/* ParkIt */}
         <Zoom triggerOnce='true'>
         <article className='reverse'>
           <div className='project-text'>
@@ -58,6 +62,30 @@ function ProjectsSection () {
               <a href="https://www.youtube.com/watch?v=eBRX8kT4M68" className='project-links' target='blank'>Video Demo</a>
           </div>
           <img src={ParkItScreenShot} alt='parkIt homepage' className='project-img'/>
+        </article>
+        </Zoom>
+        {/* The Fire-Sphere */}
+        <Zoom triggerOnce='true'>
+        <article>
+          <div className='project-text'>
+            <h4>In Progress</h4>
+            <h3>The Fire Sphere</h3>
+            <p className='textbox'>
+              The Fire Sphere is a Web Application in development for Fire Department Management. Users can log in to see shift   assignments, view daily schedules, and submit time off requests. The officers with admin access can assign the  apparatus for each shift, approve time off requests, alter schedules, and manage training requirements.
+            </p>
+            <h4>Technologies Used:</h4>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>React</li>
+                <li>Ruby on Rails</li>
+                <li>Active Record</li>
+                <li>PostgreSQL</li>
+                <li>Material-UI</li>
+              </ul>
+              <a href="https://github.com/tmills5/fire-sphere-1" className='project-links' target='blank'>GitHub Repository</a>
+          </div>
+          <img src={FireSphereScreenShot} alt='Fire Sphere homepage' className='project-img'/>
         </article>
         </Zoom>
       </section>
